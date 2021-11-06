ea867cf531a8b955e3f844c4bc9fb85697c98456
barbaha <barbaha.maria@gmail.com>
2021-11-06 03:14:25 -0300

    Migração de css da página sobre para Styled Components com responsividade da página totalmente concluida

--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -1,114 +1,81 @@
 import React from 'react'
 import NavMenu from '../../components/Menu/Menu'
 import Footer from '../../components/Footer/Footer'
-import './Sobre.css'
+import './SobreStyle.js'
+
 import ImagemPerfil1 from '../../assets/Barbaha.png'
 import ImagemPerfil2 from '../../assets/cleyton.png'
 import ImagemPerfil3 from '../../assets/eliza.png'
 import ImagemLinkedin from '../../assets/linkedin.png'
 import ImagemGithub from '../../assets/github.png'
 
+// IMPORTANDO CONST DO STYLED COMPONENTS
+
+import {
+    Container
+} from './SobreStyle.js';
+
 function Sobre() {
     return (
-        
-        <div>
+        <>
             <NavMenu />
-        <section className="container">
-
-             
-                <section className="primeiroTitulo">
-                    
-                    <h1>Sobre a Equipe Rocket</h1>
-
-                </section>
-
-                 <section className="segundoTitulo"> 
+                <Container>
+                    <header className="titulo">
+                        <h1>Sobre a Equipe Rocket</h1>
+                        <p>
+                            POKÉDEX FEITA POR TRÊS ESTUDANTES DE FRONT-END COM 
+                            ÊNFASE EM REACT COMO UM PROJETO FINAL (TCC) DO CURSO 
+                            START LATAM RECIFE.
+                        </p>
+                    </header>
+                        
+                    <section className="alunos">
+                        <section className="detalhes">
+                            <img src={ImagemPerfil1} alt="" />
+                            <h1>Barbaha Santos</h1>
+                            <div className="redesSociais">
+                                <a href="https://github.com/barbaha" className="icon">
+                                    <img alt='' src={ImagemGithub}/>  
+                                </a>
             
-            <p>POKÉDEX FEITA POR TRÊS ESTUDANTES DE FRONT-END COM ÊNFASE EM REACT COMO UM PROJETO FINAL (TCC) DO CURSO START LATAM RECIFE.</p>
-            
-            
-                </section>
-
-         </section>
-                 
-             <section className="container1">
-                    <section className="imagem1"> 
-                    
-                                 <img className="primeiraFoto" src={ImagemPerfil1} alt=''/>
-
-                                         <h3 className="nomePrimeiro">BARBAHA SANTOS </h3>
-
-            <section className="redesSociais">
-
-                          <a href="https://github.com/barbaha">
-
-                                <img className="githubFoto" alt='' src={ImagemGithub}/>  
-                            </a>
-                            
-                            <a href="https://www.linkedin.com/in/barbahasantos/">
-
-                                 <img className="linkedinFoto" alt='' src={ImagemLinkedin}/>
-                            </a>
-            </section>
-            
-                    </section>
-
-                </section>
-
-                <section className="container2">
-                    <section className="imagem2">
-                                <img className="segundaFoto" alt='' src={ImagemPerfil2}/>
-
-                                    <h3 className="nomeSegundo">CLEYTON SOUSA</h3>
-
-                <div className="redesSociais">
-
-                            <a href="https://github.com/CleytonSousa">  
-
-                                <img className="githubFoto" alt='' src={ImagemGithub}/>
-                            </a>
-
-                            <a href="https://www.linkedin.com/in/cleyton-sousa-3626a11a5/">
-
-                                <img className="linkedinFoto" alt='' src={ImagemLinkedin}/>
-                            </a>
-                </div>
-                    </section>    
-                            
-                </section>
-
-                <section className="container3">
-
-                    <section className="imagem3">
-
-                                <img className="terceiraFoto" alt='' src={ImagemPerfil3}/>
-
-                                    <h3 className="nomeTerceiro">MARIA ELIZA</h3>
-
-                <section className="redesSociais">      
-
-                            <a href="https://github.com/MariaEliiza">   
-
-                                <img className="githubFoto" alt='' src={ImagemGithub}/>
-
-                            </a>
-
-                            <a href="https://www.linkedin.com/in/maria-eliza-047765196/">
-
-                                <img className="linkedinFoto" alt='' src={ImagemLinkedin}/>
-                                
-                            </a>
-                </section>
+                                <a href="https://www.linkedin.com/in/barbahasantos/" className="icon">
+                                    <img  alt='' src={ImagemLinkedin}/>
+                                </a>
+                            </div>
                         </section>
-                    </section>
 
-               
+                        <section className="detalhes">
+                            <img src={ImagemPerfil2} alt=""/>
+                            <h1>Cleyton Sousa</h1>
+                            <div className="redesSociais">
+                                <a href="https://github.com/CleytonSousa" className="icon">
+                                    <img alt='' src={ImagemGithub}/>  
+                                </a>
+            
+                                <a href="https://www.linkedin.com/in/cleyton-sousa-3626a11a5/" className="icon">
+                                    <img alt='' src={ImagemLinkedin}/>
+                                </a>
+                            </div>
+                        </section>
 
-
-         
-           <Footer />
-        </div>    
-    
+                        <section className="detalhes">
+                            <img src={ImagemPerfil3} alt=""/>
+                            <h1>Maria Eliza</h1>
+                            <div className="redesSociais">
+                                <a href="https://github.com/MariaEliiza" className="icon">
+                                    <img alt='' src={ImagemGithub}/>  
+                                </a>
+            
+                                <a href="https://www.linkedin.com/in/maria-eliza-047765196/" className="icon">
+                                    <img alt='' src={ImagemLinkedin}/>
+                                </a>
+                            </div>
+                        </section>
+                    </section>  
+                        
+                </Container>
+            <Footer />
+        </>    
     )
 }
 
