a9dc09e9bb63733fb1f1c4f506699af4223df643
Cleyton Sousa <csdsantos@rd.com.br>
2022-02-12 20:39:48 -0300

    ✅ update footer test

--- src/components/Footer/index.spec.js
+++ src/components/Footer/index.spec.js
@@ -2,12 +2,12 @@ import { screen, render } from '@testing-library/react';
 import Footer from './Footer'
 
 describe('Should render correctly the footer', () => {
-    it('should have the team credit', () => {
+    it('should show the team credit', () => {
         render(<Footer />)
         const expected = screen.getByText(/Desenvolvido pela Equipe Rocket/i)
         expect(expected).toBeInTheDocument()
     })
-    it('should have the school name', () => {
+    it('should show the school name', () => {
         render(<Footer />)
         const expected = screen.getByText(/start/i)
         expect(expected).toBeInTheDocument()
